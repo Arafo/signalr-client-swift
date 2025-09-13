7bba94f88d6af74e91f4ebefdab8062c2666747b
Rafa Marcén <rafa_marcen@hotmail.com>
2025-09-13 21:27:38 +0200

    Update WebSocketTransport.swift

--- Package.swift
+++ Package.swift
@@ -11,11 +11,16 @@ let package = Package(
         .library(name: "SignalRClient", targets: ["SignalRClient"])
     ],
     dependencies: [
+        .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.16.1"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.86.0")
     ],
     targets: [
         .target(
             name: "SignalRClient",
             dependencies: [
+                .product(name: "WebSocketKit", package: "websocket-kit"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio")
             ]
         ),
         .testTarget(
--- Sources/SignalRClient/Transport/WebSocketTransport.swift
+++ Sources/SignalRClient/Transport/WebSocketTransport.swift
@@ -5,6 +5,9 @@ import Foundation
 #if canImport(FoundationNetworking)
     import FoundationNetworking
 #endif
+import WebSocketKit
+import NIOCore
+import NIOPosix
 
 actor WebSocketTransport: Transport {
     private let logger: Logger
@@ -77,30 +80,245 @@ actor WebSocketTransport: Transport {
         func onClose(_ handler: OnCloseHander?) async
     }
 
-    #if os(Linux)
-        private actor DefaultWebSocketConnection: WebSocketConnection {
-            func connect(request: URLRequest, transferFormat: TransferFormat) async throws {
-                throw SignalRError.unsupportedTransport("WebSockets transport is not supported on Linux")
-            }
+#if os(Linux)
+    private actor DefaultWebSocketConnection: WebSocketConnection {
+        private let logger: Logger
+        private let openTcs: TaskCompletionSource<Void> = TaskCompletionSource()
+        private let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+        private var websocket: WebSocket?
+        private var onReceive: OnReceiveHandler?
+        private var onClose: OnCloseHander?
+        
+        init(
+            logger: Logger
+        ) {
+            self.logger = logger
+        }
+        
+        func connect(
+            request: URLRequest,
+            transferFormat: TransferFormat
+        ) async throws {
+            let (url, headers) = try websocketURLStringAndHeaders(from: request)
+            let config = WebSocketClient.Configuration(maxFrameSize: Int(Int32.max) - 1)
+            logger.log(level: .debug, message: "*** Test ***")
+            logger.log(level: .debug, message: "(WebSockets transport) Connecting to \(url) with transfer format \(transferFormat).")
+            //            try await withCheckedThrowingContinuation { (cont: CheckedContinuation<Void, Error>) in
+            do {
+            try await WebSocket.connect(
+                to: url,
+                headers: headers,
+                //                    proxy: "127.0.0.1",
+                //                    proxyPort: 9090,
+                configuration: config,
+                on: group
+            ) { [weak self] socket in
+                guard let self = self else { return }
+                self.websocket = socket
 
-            func send(_ data: StringOrData) async throws {
-                throw SignalRError.unsupportedTransport("WebSockets transport is not supported on Linux")
+                logger.log(level: .debug, message: "(WebSockets transport) urlSession didOpenWithProtocol invoked. WebSocket open")
+                
+                Task {
+                    if await self.openTcs.trySetResult(.success(())) == true {
+                        self.logger.log(level: .debug, message: "(WebSockets transport) WebSocket connected")
+                    }
+                }
+                
+                socket.onText { [weak self] _, text in
+                    guard let self = self else { return }
+                    await self.onReceive?(.string(text))
+                    //Task { await self.onReceive?(.string(text)) }
+                }
+                socket.onBinary { [weak self] _, buffer in
+                    guard let self = self else { return }
+                    let data = Data(buffer.readableBytesView)
+                    await self.onReceive?(.data(data))
+                    //Task { await self.onReceive?(.data(data)) }
+                }
+                
+                socket.onClose.whenComplete { [weak self] result in
+                    guard let self = self else { return }
+                    logger.log(level: .debug, message: "(WebSockets transport) WebSocket closed")
+                    Task {
+                        switch result {
+                        case .success:
+                            await self.onClose?(nil)
+                        case .failure(let error):
+                            await self.onClose?(error)
+                        }
+                    }
+                }
+            }.get()
+            //.wait()
+            } catch {
+                logger.log(level: .error, message: "(WebSockets transport) WebSocket connect error")
+                //try? await stop(error: error)
+                            if await openTcs.trySetResult(.failure(error ?? SignalRError.connectionAborted)) == true {
+                //receiveTask?.cancel() // Cancel the receive task
+            } else {
+                //await receiveTask?.value // Wait for the receive task to complete
+                await onClose?(error) // Call the close handler
             }
-
-            func stop(error: (any Error)?) async throws {
-                throw SignalRError.unsupportedTransport("WebSockets transport is not supported on Linux")
             }
-
-            func onReceive(_ handler: WebSocketTransport.OnReceiveHandler?) async {
+            //                .whenComplete { result in
+            //                    switch result {
+            //                    case .success:
+            //                        cont.resume(returning: ())
+            //                    case .failure(let error):
+            //                        cont.resume(throwing: error)
+            //                    }
+            //                }
+            //            }
+            
+            // wait for startTcs to be completed before returning from connect
+            // this is to ensure that the connection is truely established
+            try await openTcs.task()
+        }
+        
+        func send(_ data: StringOrData) async throws {
+            guard let websocket = self.websocket else {
+                throw SignalRError.invalidOperation("Not connected")
             }
-
-            func onClose(_ handler: WebSocketTransport.OnCloseHander?) async {
-            }
-
-            init(logger: Logger) {
+            
+            switch data {
+            case .string(let s):
+                try await withCheckedThrowingContinuation { (cont: CheckedContinuation<Void, Error>) in
+                    let promise = websocket.eventLoop.makePromise(of: Void.self)
+                    websocket.send(s, promise: promise)
+                    promise.futureResult.whenComplete { result in
+                        switch result {
+                        case .success: cont.resume(returning: ())
+                        case .failure(let error): cont.resume(throwing: error)
+                        }
+                    }
+                }
+                
+            case .data(let d):
+                try await withCheckedThrowingContinuation { (cont: CheckedContinuation<Void, Error>) in
+                    var buffer = ByteBufferAllocator().buffer(capacity: d.count)
+                    buffer.writeBytes(d)
+                    let promise = websocket.eventLoop.makePromise(of: Void.self)
+                    websocket.send(buffer, opcode: .binary, promise: promise)
+                    promise.futureResult.whenComplete { result in
+                        switch result {
+                        case .success: cont.resume(returning: ())
+                        case .failure(let error): cont.resume(throwing: error)
+                        }
+                    }
+                }
             }
         }
-    #else
+        
+        func stop(error: Error?) async throws {
+            logger.log(level: .debug, message: "(WebSockets transport) Stopping")
+            if let websocket = self.websocket {
+                try await withCheckedThrowingContinuation { (cont: CheckedContinuation<Void, Error>) in
+                    let promise = websocket.eventLoop.makePromise(of: Void.self)
+                    websocket.close(promise: promise)
+                    promise.futureResult.whenComplete { result in
+                        switch result {
+                        case .success: cont.resume(returning: ())
+                        case .failure(let e): cont.resume(throwing: e)
+                        }
+                    }
+                }
+            }
+
+            if await openTcs.trySetResult(.failure(error ?? SignalRError.connectionAborted)) == true {
+                //receiveTask?.cancel() // Cancel the receive task
+            } else {
+                //await receiveTask?.value // Wait for the receive task to complete
+                await onClose?(error) // Call the close handler
+            }
+            
+            try await withCheckedThrowingContinuation { (cont: CheckedContinuation<Void, Error>) in
+                group.shutdownGracefully { err in
+                    if let err { cont.resume(throwing: err) } else { cont.resume(returning: ()) }
+                }
+            }
+        }
+        
+        func onReceive(_ handler: OnReceiveHandler?) async {
+            onReceive = handler
+        }
+        
+        func onClose(_ handler: OnCloseHander?) async {
+            onClose = handler
+        }
+        
+        private func websocketURLStringAndHeaders(from request: URLRequest) throws -> (String, HTTPHeaders) {
+            guard var comps = URLComponents(url: try urlOrThrow(request), resolvingAgainstBaseURL: false) else {
+                throw URLError(.badURL)
+            }
+            
+            switch comps.scheme?.lowercased() {
+            case "http":  comps.scheme = "ws"
+            case "https": comps.scheme = "wss"
+            case "ws", "wss": break
+            default: throw URLError(.unsupportedURL)
+            }
+            
+            if comps.path.isEmpty { comps.path = "/" }
+            
+            var headers = HTTPHeaders()
+            let hopHeaders: Set<String> = [
+                "upgrade","connection","sec-websocket-version","sec-websocket-key","host"
+            ]
+            for (k, v) in (request.allHTTPHeaderFields ?? [:]) {
+                if !hopHeaders.contains(k.lowercased()) {
+                    headers.add(name: k, value: v)
+                }
+            }
+            
+            if headers.first(name: "Accept") == nil {
+                headers.add(name: "Accept", value: "*/*")
+            }
+            if headers.first(name: "Accept-Encoding") == nil {
+                headers.add(name: "Accept-Encoding", value: "gzip, deflate")
+            }
+            if headers.first(name: "Accept-Language") == nil {
+                let acceptLanguage = Locale.preferredLanguages.enumerated()
+                    .map { i, lang in i == 0 ? lang : "\(lang);q=\(String(format: "%.1f", 1.0 - Double(i) * 0.1))" }
+                    .joined(separator: ", ")
+                headers.add(name: "Accept-Language", value: acceptLanguage)
+            }
+            
+            let existingCookie = headers.first(name: "Cookie")
+            if let cookieValue = mergedCookieHeader(for: comps.url!, existing: existingCookie) {
+                if existingCookie != nil {
+                    headers.replaceOrAdd(name: "Cookie", value: cookieValue)
+                } else {
+                    headers.add(name: "Cookie", value: cookieValue)
+                }
+            }
+            
+            guard let urlString = comps.string else { throw URLError(.badURL) }
+            return (urlString, headers)
+        }
+        private func urlOrThrow(_ request: URLRequest) throws -> URL {
+            if let url = request.url { return url }
+            throw URLError(.badURL)
+        }
+        
+        private func mergedCookieHeader(for url: URL, existing: String?) -> String? {
+            let stored = HTTPCookieStorage.shared.cookies(for: url) ?? []
+            let storageHeader = HTTPCookie.requestHeaderFields(with: stored)["Cookie"]
+            
+            switch (existing?.trimmingCharacters(in: .whitespacesAndNewlines), storageHeader) {
+            case let (e?, s?) where !e.isEmpty && !s.isEmpty:
+                return e == s ? e : "\(e); \(s)"
+            case let (e?, _) where !e.isEmpty:
+                return e
+            case let (_, s?):
+                return s
+            default:
+                return nil
+            }
+        }
+    }
+
+#else
+
         private actor DefaultWebSocketConnection: NSObject, WebSocketConnection, URLSessionWebSocketDelegate {
             private let logger: Logger
             private let openTcs: TaskCompletionSource<Void> = TaskCompletionSource()
@@ -205,7 +423,7 @@ actor WebSocketTransport: Transport {
             private func receiveMessage() async {
                 guard let websocket: URLSessionWebSocketTask = websocket else {
                     logger.log(level: .error, message: "(WebSockets transport) WebSocket is nil")
-                    return 
+                    return
                 }
 
                 do {
